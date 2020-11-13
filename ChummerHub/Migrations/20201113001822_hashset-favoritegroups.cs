b16add5f337e491b3564a8bc0497456f02643950
Delnar_Ersike <delnar.ersike@gmail.com>
2020-11-13 18:13:24 +0100

    Added Migration for HashSet Stuff

---
+++ ChummerHub/Migrations/20201113001822_hashset-favoritegroups.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChummerHub.Migrations
+{
+    public partial class hashsetfavoritegroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserFavoriteGroup");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserFavoriteGroup",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ApplicationUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    FavoriteGuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserFavoriteGroup", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserFavoriteGroup_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserFavoriteGroup_ApplicationUserId",
+                table: "ApplicationUserFavoriteGroup",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserFavoriteGroup_FavoriteGuid",
+                table: "ApplicationUserFavoriteGroup",
+                column: "FavoriteGuid");
+        }
+    }
+}
